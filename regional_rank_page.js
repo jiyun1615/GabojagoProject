abcbb67fd785c6f4088bd585ba4f29a1341b51a9
JeonJiHyeon <angry_smile@naver.com>
2022-07-18 06:55:13 +0900

    style : 지도 아이콘 만듦.

---
+++ regional_rank_page.js
@@ -0,0 +1,7 @@
+var land = document.querySelector('.land');
+
+land.addEventListener("click", function() {
+//   land.setAttribute("transform", "translate(100, 100)");
+//   클릭하면 나오는 이벤트를 설정한다. 
+    alert("yeah!");
+});
