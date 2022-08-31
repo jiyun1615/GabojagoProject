3b4077a649bcec02db213e364a77281884fdf3d0
JeonJiHyeon <angry_smile@naver.com>
2022-08-31 19:20:14 +0900

    fix

--- js/post_view_page.js
+++ js/post_view_page.js
@@ -3,15 +3,101 @@ console.log(decodeURI(receivedData));
 var img_area = document.getElementsByClassName('img_area');
 var time_post = 0;
 var time_comment = 0;
-
+//글 작성자와 댓글 작성자, 이 둘은 본인확인이 필요함. => 수정 혹은 삭제시에!
 
 console.log(window.sessionStorage.getItem("JWT"));
 if (window.sessionStorage.getItem("JWT") != null) {
   user_view();
+  document.getElementById('heart_obj').onload = e => {
+    const obj = document.getElementById('heart_obj');
+    const query = document.querySelector('#heart_obj');
+    console.log(query);
+
+    const querydoc = query.contentDocument;
+    console.log(querydoc);
+
+    if (obj.getAttribute('data') == "..\\icons\\like_heart_blank.svg") {
+      //좋아요 api
+      const lands = querydoc.querySelectorAll(".heart_blank")
+        .forEach((element) =>
+          element.addEventListener("click", function () {
+            console.log("클릭은 잘 됩니다");
+            obj.setAttribute('data', "..\\icons\\like_heart.svg")
+
+            $.ajax({
+              type: "POST",
+              url: "http://13.209.87.88:8080/posts/great/" + decodeURI(receivedData),
+              headers: { Authorization: window.sessionStorage.getItem("JWT") },
+              data: {},
+              success: function (response) {
+                console.log(response);
+                location.reload();
+              }
+            })
+
+          }));
+    }
+
+    else if (obj.getAttribute('data') == "..\\icons\\like_heart.svg") {
+      //좋아요 삭제 api
+      const lands = querydoc.querySelectorAll(".heart_fill")
+        .forEach((element) =>
+          element.addEventListener("click", function () {
+            console.log("클릭은 잘 됩니다");
+            obj.setAttribute('data', "..\\icons\\like_heart_blank.svg");
+
+            $.ajax({
+              type: "DELETE",
+              url: "http://13.209.87.88:8080/posts/great/" + decodeURI(receivedData),
+              headers: { Authorization: window.sessionStorage.getItem("JWT") },
+              data: {},
+              success: function (response) {
+                console.log(response);
+                location.reload();
+              }
+            })
+
+          }));
+    }
+
+    $("textarea").attr('readonly', 'true');
+    $(".send-btn").click(function () {
+      swal("로그인이 필요한 서비스입니다.", "로그인 페이지로 이동합니다.", "error").then(function () {
+        window.location.href = 'login_page.html';
+      });
+    });
+
+  };
+
 }
 else if (window.sessionStorage.getItem("JWT") == null) {
   not_user_view();
+  document.getElementById('heart_obj').onload = e => {
+    const obj = document.getElementById('heart_obj');
+    const query = document.querySelector('#heart_obj');
+    console.log(query);
+
+    const querydoc = query.contentDocument;
+    console.log(querydoc);
+
+    const lands = querydoc.querySelectorAll(".heart_blank")
+      .forEach((element) =>
+        element.addEventListener("click", function () {
+          swal("로그인이 필요한 서비스입니다.", "로그인 페이지로 이동합니다.", "error").then(function () {
+            window.location.href = 'login_page.html';
+          });
+        }));
+  };
+  $("textarea").attr('readonly', 'true');
+  $("textarea").click(function () {
+    swal("로그인이 필요한 서비스입니다.", "로그인 페이지로 이동합니다.", "error").then(function () {
+      window.location.href = 'login_page.html';
+    });
+  });
 }
+
+
+
 
 function user_view() {
   $.ajax({
@@ -20,6 +106,7 @@ function user_view() {
     headers: { Authorization: window.sessionStorage.getItem("JWT") },
     data: {},
     success: function (response) {
+      console.log(response);
       $("#Author").text(response.user.name);
       console.log("게시글 시간 : " + response.createdAt);
       time_post = response.createdAt;
@@ -31,7 +118,11 @@ function user_view() {
       $("#viewcnt").text(response.viewCnt);
       $(".text_area").text(response.context);
       $("#likecnt").text("+ " + response.greatCnt);
-
+      var tags = "";
+      for (var i = 0; i < response.postTags.length; i++) {
+        tags = tags + "#" + response.postTags[i].value + " ";
+      }
+      $(".tag_area").text(tags);
 
       if (response.files.length == 0) img_area[0].style.display = "none";
       else {
@@ -87,57 +178,63 @@ function user_view() {
 
 function not_user_view() {
   //비회원일때... - 헤더에 토큰 필요 없음
-$.ajax({
-  type: "GET",
-  url: "http://13.209.87.88:8080/posts/" + decodeURI(receivedData),
-  data: {},
-  success: function (response) {
-    $("#Author").text(response.user.name);
-    console.log("게시글 시간 : " + response.createdAt);
-    time_post = response.createdAt;
-    var createdAt_index = time_post.indexOf('T');
-    var createdAt = time_post.substr(0, createdAt_index);
+  $.ajax({
+    type: "GET",
+    url: "http://13.209.87.88:8080/posts/" + decodeURI(receivedData),
+    data: {},
+    success: function (response) {
+      console.log(response);
+      $("#Author").text(response.user.name);
+      console.log("게시글 시간 : " + response.createdAt);
+      time_post = response.createdAt;
+      var createdAt_index = time_post.indexOf('T');
+      var createdAt = time_post.substr(0, createdAt_index);
 
-    $("#createdAt").text(createdAt);
-    $("#title").text(response.title);
-    $("#viewcnt").text(response.viewCnt);
-    $(".text_area").text(response.context);
-    $("#likecnt").text("+ " + response.greatCnt);
+      $("#createdAt").text(createdAt);
+      $("#title").text(response.title);
+      $("#viewcnt").text(response.viewCnt);
+      $(".text_area").text(response.context);
+      $("#likecnt").text("+ " + response.greatCnt);
 
-
-    if (response.files.length == 0) img_area[0].style.display = "none";
-    else {
-      for (var i = 0; i < response.files.length; i++) {
-        var tmpHtml = `<img src="${response.files[i].filePath}">`
-        $(".img_area").append(tmpHtml);
+      var tags = "";
+      for (var i = 0; i < response.postTags.length; i++) {
+        tags = tags + "#" + response.postTags[i].value + " ";
       }
+      $(".tag_area").text(tags);
+
+      if (response.files.length == 0) img_area[0].style.display = "none";
+      else {
+        for (var i = 0; i < response.files.length; i++) {
+          var tmpHtml = `<img src="${response.files[i].filePath}">`
+          $(".img_area").append(tmpHtml);
+        }
+      }
+
+
     }
+  })
 
 
-  }
-})
+  //비회원일때... - 헤더에 토큰 필요 없음
+  $.ajax({
+    type: "GET",
+    url: "http://13.209.87.88:8080/comments/post/" + decodeURI(receivedData) + "?page=1&size=10",
+    data: {},
+    success: function (response) {
+      console.log(response)
+
+      for (var i = 0; i < response.comments.length; i++) {
+        time_comment = response.comments[i].createdAt;
+
+        console.log("댓글 시간 : " + response.comments[i].createdAt);
+
+        //글 시간하고 계산하면 잘 나올거같은데...
+        var createdAt_index = time_comment.indexOf('T');
+        var createdAt_index_end = time_comment.indexOf('.');
+        var createdAt = time_comment.substr(createdAt_index + 1, createdAt_index_end);
 
 
-//비회원일때... - 헤더에 토큰 필요 없음
-$.ajax({
-  type: "GET",
-  url: "http://13.209.87.88:8080/comments/post/" + decodeURI(receivedData) + "?page=1&size=10",
-  data: {},
-  success: function (response) {
-    console.log(response)
-
-    for (var i = 0; i < response.comments.length; i++) {
-      time_comment = response.comments[i].createdAt;
-
-      console.log("댓글 시간 : " + response.comments[i].createdAt);
-
-      //글 시간하고 계산하면 잘 나올거같은데...
-      var createdAt_index = time_comment.indexOf('T');
-      var createdAt_index_end = time_comment.indexOf('.');
-      var createdAt = time_comment.substr(createdAt_index + 1, createdAt_index_end);
-
-
-      var tmpHtml = `<a class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
+        var tmpHtml = `<a class="list-group-item list-group-item-action d-flex gap-3 py-3" aria-current="true">
             <img src="${response.comments[i].profilePhoto}" width="32" height="32"
               class="rounded-circle flex-shrink-0">
             <div class="d-flex gap-2 w-100 justify-content-between">
@@ -148,26 +245,11 @@ $.ajax({
               <small class="opacity-50 text-nowrap">${createdAt}</small>
             </div>
           </a>`
-      $("#comment_area").append(tmpHtml);
+        $("#comment_area").append(tmpHtml);
+      }
     }
-  }
 
 
 
-})
+  })
 }
-
-
-
-
-const query = document.querySelector('#heart_obj');
-console.log(query);
-
-const querydoc = query.contentDocument;
-console.log(querydoc);
-
-const lands = querydoc.querySelectorAll(".heart_blank")
-  .forEach((element) =>
-    element.addEventListener("click", function () {
-      console.log("클릭은 잘 됩니다");
-    }));
