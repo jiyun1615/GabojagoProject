aade81aa3332354f4475b2ea640b30847195f5b0
JeonJiHyeon <angry_smile@naver.com>
2022-09-03 09:52:34 +0900

    fix : 태그별 페이지 개선

--- js/tag_page.js
+++ js/tag_page.js
@@ -1,62 +1,75 @@
 function btn_onclick(num) {
-    callApiInfo(num,"1");
+    callApiInfo(num, "1");
     tagChange(num);
 }
 
 function dropdown_onclick(num) {
-    callApiInfo(num,"1");
-    noneTag(num);
+    switch(num){
+        case 1 : 
+        $("#sort_by").text("태그별");
+        break;
+
+        case 20 : 
+        $("#sort_by").text("조회순");
+        break;
+
+        case 21 : 
+        $("#sort_by").text("북마크");
+        break;
+    }
+    callApiInfo(num, "1");
+    noneTag(num); //태그 안보이게.
 
 }
 
 var totalPage = 0;
-var tagNum=0;
+var tagNum = 0;
 
 function pageBtn_onclick(num) {
-    // pageBtnChange(num);
     callApiInfo(tagNum, num);
 }
 
 function callApiInfo(num, pageNumber) {
     tagNum = num;
-    console.log("callApiInfo : ");
-    if(num==20) {
+    if (num == 20) {
         //num=20 조회순 페이지
-        console.log("callAPI20");
-        url1="http://13.209.87.88:8080/hotplaces?page=" + pageNumber + "&size=12";
-    } else if (num==21) {
-        //num=20 북마크순 페이지
-        url1="http://13.209.87.88:8080/hotplaces/bookmark?page=" + pageNumber + "&size=5";
+        apiurl = "http://13.209.87.88:8080/hotplaces?page=" + pageNumber + "&size=12";
+    } else if (num == 21) {
+        //num=21 북마크순 페이지
+        apiurl = "http://13.209.87.88:8080/hotplaces/bookmark?page=" + pageNumber + "&size=12";
     } else {
         //num=1~11 태그별 페이지
-        url1="http://13.209.87.88:8080/hotplaces/tag/" + num + "?page=" + pageNumber + "&size=12";
+        apiurl = "http://13.209.87.88:8080/hotplaces/tag/" + num + "?page=" + pageNumber + "&size=12";
     }
     $.ajax({
-        url: url1,
+        url: apiurl,
         type: "GET",
         data: "json",
         success: function (data) {
             var response;
-            if(num<12) {
+
+            if (num < 12) { //num=1~11 태그별 페이지
                 response = data.spotResponses;
-            } else if(num== 20) {
+            } else if (num == 20) { //num=20 조회순 페이지
+                response = data.spotResponses;
+            } else {//num=21 북마크순 페이지
                 response = data.spotBookmarkResponses;
-            } else //num==21 조회순 페이지
-            {
-                response = data.spotResponses;
             }
             var dataCnt = response.length;
-
             $("#exampleArr").empty();
             $("#pageNum").empty();
             totalPage = data.totalPages;
 
             for (var i = 0; i < dataCnt; i++) {
+
                 var spotID = response[i].spotId;
                 var name = response[i].spotName;
-                if (response[i].detail == "None") var details = "상세 정보 페이지를 확인해 주세요.";
+                if (response[i].detail == "None" || response[i].detail == "none" || response[i].detail == null) var details = "상세 정보 페이지를 확인해 주세요.";
                 else var details = response[i].detail;
-                var img1 = response[i].spotImage;
+
+                if (response[i].spotImage == "None" || response[i].spotImage == "none" || response[i].spotImage == null) var img1 = "..\\sampleimages\\sample_img.png"
+                else var img1 = response[i].spotImage;
+
                 var viewCnt = response[i].viewCnt;
                 var tmpHtml = `<div class="col">
                                 <div class = "card1">
@@ -78,10 +91,10 @@ function callApiInfo(num, pageNumber) {
                 $("#exampleArr").append(tmpHtml);
             }
             for (var i = 1; i <= totalPage; i++) {
-                var tmpHtml1 = `<button type="radio" class="pageBtn" id="pageBtn${i}" onclick="pageBtn_onclick(${i})">${i}</button>`
+                if(i==pageNumber) var tmpHtml1 = `<button type="radio" class="pageBtn" id="pageBtn${i}" onclick="pageBtn_onclick(${i})" style="border-bottom:1px solid gray">${i}</button>`
+                else var tmpHtml1 = `<button type="radio" class="pageBtn" id="pageBtn${i}" onclick="pageBtn_onclick(${i})">${i}</button>`
                 $("#pageNum").append(tmpHtml1);
             }
-
 
         },
         error: (log) => { alert("url: " + url) }
@@ -95,7 +108,7 @@ function detailPage(num) {
 }
 
 function tagChange(num) {
-
+    // html에 ready function을 body 상단에 두니 첫실행땐 널포인터가 되어서 버튼 색이 안변함! 그래서 하단으로 내려줌!
     for (i = 1; i <= 11; i++) {
         const btnElement1 = document.getElementById("btn" + i);
         btnElement1.style.backgroundColor = "white";
@@ -108,27 +121,31 @@ function tagChange(num) {
 }
 
 
-//버튼 클릭시 디자인 pageNum
-function pageBtnChange(num) {
+// 버튼 클릭시 디자인 pageNum
+// 아무래도 이부분은 페이지를 선택하면 ajax로 덮어씌워지는것같아서 아예 ajax 부를때 비교하도록 바꿨어!
+// function pageBtnChange(num) {
+//     console.log("페이지 클릭 pageBtn : " + num);
+//     const btnElement1 = document.getElementById("pageBtn" + num);
+//     btnElement1.style.borderBottom = "1px solid #000000";
 
-    for (i = 0; i <= totalPage; i++) {
-        if (i != num) {
-            const btnElement = document.getElementById("pageBtn" + i);
-            btnElement.style.borderColor="white";
-        }   
-    }
+//     for (i = 1; i <= totalPage; i++) {
+//         if (i != num) {
+//             console.log("페이지 클릭 pageBtn : " + num + " i는 "+ i);
+//             const btnElement = document.getElementById("pageBtn" + i);
+//             btnElement.style.borderColor = "white";
+//         }
+//     }
 
-    const btnElement1 = document.getElementById("pageBtn" + num);
-    btnElement1.style.borderBottom="1px solid gray";
 
-}
+
+// }
+
 
 function noneTag(num) {
-    if(num>12)
-    {
-        document.getElementById("tagButton").style.display="none";
+    if (num > 12) {
+        document.getElementById("tagButton").style.display = "none";
     }
     else {
-        document.getElementById("tagButton").style.display="";
+        document.getElementById("tagButton").style.display = "";
     }
 }
