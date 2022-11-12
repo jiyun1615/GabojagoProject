301b11ac0acb2c2ad775ad454ddbce6d21a1514f
jH <dsdppp@naver.com>
2022-11-12 17:04:33 +0900

    feat : 리뷰 작성 기능 및, 상세조회 화면에서 리뷰 노출

--- js/hotPlaceDetail.js
+++ js/hotPlaceDetail.js
@@ -1,14 +1,64 @@
 var num;
 function callApiInfo() {
+
     const urlParams = (window.location.search);
     num = urlParams.split('=')[1];
     console.log(num);
+
+    $.ajax({
+        url: "http://13.209.87.88:8080/hotplaces/bookmark/" + num,
+        type: "GET",
+        headers: { Authorization: window.sessionStorage.getItem("JWT") },
+        success: function (data) {
+            console.log(data)
+            if (data) {
+                $("#bookmarkBtn").removeClass("bookmark_btn");
+                $("#bookmarkBtn").addClass("bookmark_btn_checked");
+                $("#bookmarkBtn").html("북마크 완료");
+            }
+            else {
+                $("#bookmarkBtn").removeClass("bookmark_btn_checked");
+                $("#bookmarkBtn").addClass("bookmark_btn");
+                $("#bookmarkBtn").html("북마크 하기");
+            }
+        },
+        error: (log) => { alert("실패" + log) }
+    })
+
+
+
+
+
 
     $.ajax({
         url: "http://13.209.87.88:8080/hotplaces/id/" + num,
         type: "GET",
         data: "json",
         success: function (data) {
+            if(data.posts.length == 0){
+                var tmpHtml = `
+                <div class="col-lg-4">
+                    <p>아직 등록된 리뷰가 없습니다.</p>
+                </div>`
+                $("#exampleArr").append(tmpHtml);
+            }
+            else{
+                for(var i=0; i<data.posts.length; i++){
+                    console.log(data.posts[i]);
+                    var tmpHtml = `
+                    <div class="col-lg-4">
+                        <svg class="bd-placeholder-img" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img"
+                                aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <title>Placeholder</title>
+                            <image href="${data.posts[i].files[0].filePath}" height="140" width="140"/>
+                        </svg>
+                        <h4 class="fw-normal">${data.posts[i].title}</h4>
+                        <p>${data.posts[i].context}</p>
+                        <div class="postLink_btn"><a class="btn btn-secondary" style="margin-bottom: 2vh;"onclick="location.href = \`post_view_page.html?${data.posts[i].postId}\`">View details »</a></div>
+                    </div>`
+                }
+                $("#exampleArr").append(tmpHtml);
+            }
             var spotName = data.spotName;
             var address = data.address;
             address = noneCheck(address);
@@ -61,22 +111,15 @@ function noneCheck(str) {
 function bookmarkBtn_onclick() {
     login_check();
     if ($("#bookmarkBtn").css("color") == "rgb(0, 0, 0)") {
-        $("#bookmarkBtn").removeClass("bookmark_btn");
-        $("#bookmarkBtn").addClass("bookmark_btn_checked");
-        $("#bookmarkBtn").html("북마크 완료");
-        
         callBookMarkApi(num);
     } else {
-        $("#bookmarkBtn").removeClass("bookmark_btn_checked");
-        $("#bookmarkBtn").addClass("bookmark_btn");
-        $("#bookmarkBtn").html("북마크 하기");
         returnBookMarkApi(num);
     }
 }
 
 function callBookMarkApi(num) {
     console.log("bookmarkApi num : " + num);
-    var bookMarkData = {"spotId": Number(num)};
+    var bookMarkData = { "spotId": Number(num) };
     console.log("bookMarkData = " + JSON.stringify(bookMarkData));
     $.ajax({
         type: "POST",
@@ -108,11 +151,10 @@ function returnBookMarkApi(num) {
 
 var checkCnt;
 function postBtn_onclick() {
-    checkCnt=0;
+    checkCnt = 0;
     login_check();
-    if(checkCnt == 0)
-    {
-        window.location.href = 'new_post_page.html?spotID=' + num;
+    if (checkCnt == 0) {
+        window.location.href = 'new_post_page.html?' + num;
     }
 }
 
