6b9dddf31acf94d646bd7efa1d44b5c94edfe12c
JeonJiHyeon <angry_smile@naver.com>
2022-11-15 22:23:37 +0900

    배포용 api 적용완료

--- js/oauth.js
+++ js/oauth.js
@@ -3,7 +3,7 @@
 
 const CLIENT_ID = "90d4e2e23eca15d423ba5a452f493217";
 
-const REDIRECT_URI =  "http://127.0.0.1:5501/html/login_redirect.html";
-// const REDIRECT_URI =  "http://13.209.87.88:80/login_redirect.html";
+// const REDIRECT_URI =  "http://127.0.0.1:5501/html/login_redirect.html";
+const REDIRECT_URI =  "http://13.209.87.88:80/login_redirect.html";
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
